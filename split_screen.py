3396c52dea906393ab62007a6fa85525d525ee05
Isobel Smith <isobel@bridesmaid.org>
2016-08-04 15:04:46 +0100

    opened the leader link in the second iframe

--- split_screen.py
+++ split_screen.py
@@ -6,12 +6,20 @@ app = Flask(__name__)
 def index():
    return render_template('index.html')
 
-@app.route('/<session_id>')
+@app.route('/splitscreen')
+def splitscreen():
+   return render_template('splitscreen.html')
+
+@app.route('/splitscreen/<session_id>')
 def index_session_id(session_id):
    return render_template('follower_side.html')
 
+@app.route('/splitscreen/follower_leader_frame')
+def splitscreen_test():
+   return render_template('follower_leader_frame.html')
 
 @app.route('/first_frame')
 def first_frame():
    return render_template('first_frame.html')
+
 
